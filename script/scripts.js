46bea63d3c83fb4433e468223d32e65b82b875b4
Uri7337 <109479378+Uri7337@users.noreply.github.com>
2023-06-02 08:09:29 +0200

    Update scripts.js
    added few features

--- script/scripts.js
+++ script/scripts.js
@@ -1,11 +1,13 @@
 function calculateTime() {
+    var startDate = new Date();
     var startTime = document.getElementById("start-time").value;
+    var endDate = document.getElementById("end-date").value;
     var endTime = document.getElementById("end-time").value;
 
-    var start = new Date("2020-01-01T" + startTime + ":00");
-    var end = new Date("2020-01-01T" + endTime + ":00");
+    var startDateTime = new Date(startDate.toDateString() + " " + startTime);
+    var endDateTime = new Date(endDate + " " + endTime);
 
-    var diff = end - start;
+    var diff = endDateTime - startDateTime;
 
     var hours = Math.floor(diff / 1000 / 60 / 60);
     diff -= hours * 1000 * 60 * 60;

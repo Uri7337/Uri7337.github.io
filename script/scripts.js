539d6e1387815805afbcb3810e2329f0c864f23c
Uri7337 <109479378+Uri7337@users.noreply.github.com>
2023-08-02 12:27:42 +0200

    Update scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -1,4 +1,5 @@
 function calculateTimeDifference() {
+      const startDateInput = document.getElementById("start-date").value;
       const startTimeInput = document.getElementById("start-time").value;
       const [startHour, startMinute] = startTimeInput.split(":").map(Number);
 
@@ -8,8 +9,19 @@ function calculateTimeDifference() {
       const endMinute = 20;
 
       // Create Date objects for start time and end time
-      const startTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), startHour, startMinute);
-      const endTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), endHour, endMinute);
+      const startTime = new Date(startDateInput);
+      startTime.setHours(startHour, startMinute);
+
+      const endTime = new Date(startTime);
+      if (startTime < today) {
+        // If the start time is in the past, set the end time to today at 16:20
+        endTime.setHours(endHour, endMinute);
+        document.getElementById("end-date").value = today.toISOString().slice(0, 10);
+      } else {
+        // Otherwise, set the end time to the same date as the start time at 16:20
+        endTime.setHours(endHour, endMinute);
+        document.getElementById("end-date").value = startDateInput;
+      }
 
       // Calculate the time difference in milliseconds
       const timeDifferenceInMs = endTime - startTime;

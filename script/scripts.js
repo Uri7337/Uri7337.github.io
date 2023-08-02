502a40e0aea6ab0ceb2eee56e9ad67cd7b0bc022
Uri7337 <109479378+Uri7337@users.noreply.github.com>
2023-08-02 12:32:41 +0200

    Update scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -1,36 +1,29 @@
+// scripts.js
 function calculateTimeDifference() {
-      const startDateInput = document.getElementById("start-date").value;
-      const startTimeInput = document.getElementById("start-time").value;
-      const [startHour, startMinute] = startTimeInput.split(":").map(Number);
+  const startTimeInput = document.getElementById("start-time").value;
+  const [startHour, startMinute] = startTimeInput.split(":").map(Number);
 
-      // Get today's date and 16:20 as end time
-      const today = new Date();
-      const endHour = 16;
-      const endMinute = 20;
+  const endTimeInput = document.getElementById("end-time").value;
+  const [endHour, endMinute] = endTimeInput.split(":").map(Number);
 
-      // Create Date objects for start time and end time
-      const startTime = new Date(startDateInput);
-      startTime.setHours(startHour, startMinute);
+  // Get today's date
+  const today = new Date();
 
-      const endTime = new Date(startTime);
-      if (startTime < today) {
-        // If the start time is in the past, set the end time to today at 16:20
-        endTime.setHours(endHour, endMinute);
-        document.getElementById("end-date").value = today.toISOString().slice(0, 10);
-      } else {
-        // Otherwise, set the end time to the same date as the start time at 16:20
-        endTime.setHours(endHour, endMinute);
-        document.getElementById("end-date").value = startDateInput;
-      }
+  // Create Date objects for start time and end time
+  const startTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), startHour, startMinute);
+  const endTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), endHour, endMinute);
 
-      // Calculate the time difference in milliseconds
-      const timeDifferenceInMs = endTime - startTime;
+  // Check if the end time is before the start time, then add one day to the end time
+  if (endTime < startTime) {
+    endTime.setDate(endTime.getDate() + 1);
+  }
 
-      // Convert time difference to minutes
-      const timeDifferenceInMinutes = Math.floor(timeDifferenceInMs / (1000 * 60));
-      const hours = Math.floor(timeDifferenceInMinutes / 60);
-      const minutes = timeDifferenceInMinutes % 60;
+  // Calculate the time difference in milliseconds
+  const timeDifferenceInMs = endTime - startTime;
 
-      const resultElement = document.getElementById("result");
-      resultElement.textContent = `Time difference: ${hours} hours and ${minutes} minutes or ${timeDifferenceInMinutes} minutes. `;
-    }
+  // Convert time difference to minutes
+  const timeDifferenceInMinutes = Math.floor(timeDifferenceInMs / (1000 * 60));
+
+  const resultElement = document.getElementById("result");
+  resultElement.textContent = `Time difference: ${timeDifferenceInMinutes} minutes.`;
+}

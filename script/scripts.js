0573b1938a9a2c9e23fabde3d004d8f4eb3fc725
Uri7337 <109479378+Uri7337@users.noreply.github.com>
2023-08-02 12:24:34 +0200

    Update scripts.js

--- script/scripts.js
+++ script/scripts.js
@@ -1,34 +1,24 @@
-function calculateEndTime() {
-    const startTimeInput = document.getElementById("start-time");
-    const endTimeInput = document.getElementById("end-time");
+function calculateTimeDifference() {
+      const startTimeInput = document.getElementById("start-time").value;
+      const [startHour, startMinute] = startTimeInput.split(":").map(Number);
 
-    // Get the current date
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
+      // Get today's date and 16:20 as end time
+      const today = new Date();
+      const endHour = 16;
+      const endMinute = 20;
 
-    // Get the selected start time
-    const startTime = startTimeInput.value;
+      // Create Date objects for start time and end time
+      const startTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), startHour, startMinute);
+      const endTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), endHour, endMinute);
 
-    // Set the end time to 16:20
-    const endTime = "16:20";
+      // Calculate the time difference in milliseconds
+      const timeDifferenceInMs = endTime - startTime;
 
-    // Combine the date and time strings to create the final date-time values
-    const startDateTime = `${year}-${month}-${day}T${startTime}`;
-    const endDateTime = `${year}-${month}-${day}T${endTime}`;
+      // Convert time difference to minutes
+      const timeDifferenceInMinutes = Math.floor(timeDifferenceInMs / (1000 * 60));
+      const hours = Math.floor(timeDifferenceInMinutes / 60);
+      const minutes = timeDifferenceInMinutes % 60;
 
-    // Set the calculated end time in the input field
-    endTimeInput.value = endTime;
-
-    // Display the result
-    console.log("Start Date-Time:", startDateTime);
-    console.log("End Date-Time:", endDateTime);
-}
-
-// Add an event listener to the start time input to recalculate end time on change
-document.getElementById("start-time").addEventListener("change", calculateEndTime);
-
-// Initially calculate the end time with the default value of the start time input
-
-
+      const resultElement = document.getElementById("result");
+      resultElement.textContent = `Time difference: ${hours} hours and ${minutes} minutes or ${timeDifferenceInMinutes} minutes. `;
+    }
